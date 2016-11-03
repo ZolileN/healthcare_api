0f31fbf69804f96448d539b9d2adfd2e4e5e80a7
jfirminger <joshfirminger@live.com>
2016-11-02 20:12:40 -0500

    added app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+__author__ = 'Josh Firminger'
+
+model_directory = 'model'
+model_file_name = '%s/model.pkl' % model_directory
+model_columns_file_name = '%s/model_columns.pkl' % model_directory
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except Exception, e:
+        port = 8080
+
+    try:
+        clf = joblib.load(model_file_name)
+        print 'model loaded'
+        model_columns = joblib.load(model_columns_file_name)
+        print 'model columns loaded'
+
+    except Exception, e:
+        print('No Model')
